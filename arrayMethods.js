c32338cdd437955f46142aef402cad1ae8d0ab50
Nata <38878737+NataHanguga@users.noreply.github.com>
2018-11-04 20:42:05 +0200

    Update arrayMethods.js

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,5 +1,4 @@
 
-
 var objectOfArrays = {
     FirstArray: [-34242, 675, 987, 0, 4, 8, -342],
     SecondArray: [3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2 - 3 - 1],
@@ -10,48 +9,49 @@ var objectOfArrays = {
 };
 //function which find max, min, sum of array and swap 
 //max and min elem of array and then created object
-function SortArray(arr) {
-    var min = arr[0];
-    var max = min;
-    var a;
-    var cout = 0;
-    var maxIndex;
-    var minIndex;
-    var tmp;
-    var arr1 = [];
+function sortArray(arr) {
+    var properties = {
+        min: arr[0],
+        max: this.min,
+        cout: 0,
+        maxIndex: 0,
+        minIndex: 0,
+        tmp: 0,
+        arr1: []
+    };
     //found max and min elements of array
     for (var i = 0; i < arr.length; i++) {
-        if (arr[i] > max) max = arr[i];
-        if (arr[i] < min) min = arr[i];
-        arr1[i] = arr[i];
+        if (arr[i] > properties.max) properties.max = arr[i];
+        if (arr[i] < properties.min) properties.min = arr[i];
+        properties.arr1[i] = arr[i];
     }
     //found max and min element`s indexes of array
     for (var i = 0; i < arr.length; i++) {
-        arr[i] === max ? maxIndex = i : a;
-        arr[i] === min ? minIndex = i : a;
-        cout += arr[i];
+        if (arr[i] === properties.max) properties.maxIndex = i;
+        if (arr[i] === properties.min) properties.minIndex = i;
+        properties.cout += arr[i];
     }
     //swap max and min elements of array 
-    tmp = arr[maxIndex];
-    arr[maxIndex] = arr[minIndex];
-    arr[minIndex] = tmp;
+    properties.tmp = arr[properties.maxIndex];
+    arr[properties.maxIndex] = arr[properties.minIndex];
+    arr[properties.minIndex] = properties.tmp;
     //create object
-    var SortMass = {
-        startArr: arr1,
+    var sortMass = {
+        startArr: properties.arr1,
         finishArr: arr,
-        maxElem: max,
-        minElem: min,
-        sum: cout,
+        maxElem: properties.max,
+        minElem: properties.min,
+        sum: properties.cout,
     };
-    console.log(SortMass);
-    return SortMass;
+    console.log(sortMass);
+    return sortMass;
 }
 //function with callback
-function DoThis(arr, callback) {
+function doThis(arr, callback) {
     callback(arr);
 }
 //function return object with array`s params
-function ArrToObj(arr) {
+function arrToObj(arr) {
     var object = {};
     for (var i = 0; i < arr.length; i++) {
         object[arr[i]] = i;
@@ -59,5 +59,5 @@ function ArrToObj(arr) {
     console.log(object);
     return object;
 }
-ArrToObj(objectOfArrays.FirstArray);
-DoThis(objectOfArrays.FirstArray, SortArray);
+arrToObj(objectOfArrays.FirstArray);
+doThis(objectOfArrays.FirstArray, sortArray);
